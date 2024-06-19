ef9e210d80089adaf0a92c01818386f71a8aaa96
Diya Gohel <139984932+diyagohel18@users.noreply.github.com>
2024-06-19 09:58:38 +0530

    gym management
    Developed a gym management system using advanced Java to provide efficient solutions to manage clients and maintain gym equipment.
    Implemented SQLite customer data.
    
    Signed-off-by: Diya Gohel <139984932+diyagohel18@users.noreply.github.com>

---
+++ org.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package java;
+
+/**
+ *
+ * @author DIYA GOHEL
+ */
+public class org {
+
+    public static class jdesktop {
+
+        public static Object beansbinding;
+        public static Object swingbinding;
+
+        public jdesktop() {
+        }
+
+        public static class beansbinding {
+
+            public beansbinding() {
+            }
+
+            public static class BindingGroup {
+
+                public BindingGroup() {
+                }
+            }
+        }
+
+        public static class swingbinding {
+
+            public swingbinding() {
+            }
+
+           public static class swingbinding {
+
+                public swingbinding() {
+                }
+            }
+        
+    }
+    public static class jdesktop {
+
+        public jdesktop static {
+        }
+    }
+    
+}
